914d85d8af8e3212928d08fedb60fb6b9157ac17
Lucien <root@DESKTOP-7CO9QQK>
2024-09-10 10:18:37 +0200

    adding creation account page

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,7 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.shortcuts import render
+
+def addPage(request):
+    return render(request, "api/add.html")
 
 @api_view(['GET'])
 def getData(request):
 	person = {'name':'Dennis', 'age':28}
 	return Response(person)
+
+	
