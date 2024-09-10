062ccbe47712a1ff3ae2419ef8b4fed8eb881b1a
Lucien <root@DESKTOP-7CO9QQK>
2024-09-10 20:09:11 +0200

    we can create users

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('add_user/', views.add_user),
+	path('list_users/', views.list_users),
 ]
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from django.shortcuts import render
 from .serializers import UserSerializer
 from rest_framework import status
+from .models import User
 
 @api_view(['POST'])
 def add_user(request):
@@ -11,3 +12,7 @@ def add_user(request):
         serializer.save()
         return Response({"message": "User added successfully"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def list_users(request):
+    users = User.objects.all()
+    return render(request, 'api/list_users.html', {'users': users})
