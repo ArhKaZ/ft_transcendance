417e1981ddc9203545c79c64ebeb92d2320940aa
Lucien <root@DESKTOP-7CO9QQK>
2024-09-10 11:26:02 +0200

    trying to add a new user in user/add

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('api.urls')),
-	path('user/', include('user.urls'))
+	path('user/', include('user.urls')),
+	path('home/', views.maison)
 ]
