e34e72cbb64ac48b65b3085d0ff0540d83bbabea
ArhKaZ <sylvain.arhkaz@gmail.com>
2024-10-28 12:32:56 +0100

    push for merge with l work

---
+++ Services/PF-Fighter/src/game/hitbox.js
@@ -0,0 +1,322 @@
+class Hitbox {
+    constructor(obj, dir) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        this.dir = dir;
+        this.damage = 0;
+        this.power = 1;
+        this.frameBegin = 0;
+        this.frameEnd = 0;
+        this.isActive = false;
+        this.updateHitbox(obj, true);
+    }
+
+    checkActivation(currentFrame) {
+        if (currentFrame >= this.frameBegin && currentFrame <= this.frameEnd) {
+            this.isActive = true;
+        } else {
+            this.isActive = false;
+        }
+    }
+
+    isExpired(currentFrame) {
+        return currentFrame > this.frameEnd;
+    }
+
+    updateHitbox(obj, assign = false) {
+        switch (this.dir) {
+            case 'upAirLeft1':
+                this.x = obj.cX;
+                this.y = obj.cY + 20;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 1.5;
+                if (assign) {
+                    this.frameBegin = 3;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'upAirLeft2':
+                this.x = obj.cX - 5;
+                this.y = obj.cY + 4;
+                this.width = 10;
+                this.height = 20;
+                this.damage = 13;
+                this.power = 1.5;
+                if (assign) {
+                    this.frameBegin = 4;
+                    this.frameEnd = 6;
+                }
+                break;
+            case 'upAirRight1':
+                this.x = obj.cX + obj.cWidth - 4;
+                this.y = obj.cY + 20;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 1.5;
+                if (assign) {
+                    this.frameBegin = 3;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'upAirRight2':
+                this.x = obj.cX + obj.cWidth;
+                this.y = obj.cY + 4;
+                this.width = 10;
+                this.height = 20;
+                this.damage = 13;
+                this.power = 1.5;
+                if (assign) {
+                    this.frameBegin = 4;
+                    this.frameEnd = 6;
+                }
+                break;
+            case 'downAirLeft1':
+                this.x = obj.cX - 10;
+                this.y = obj.cY + (obj.cHeight / 2) - 10;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 20;
+                this.power = 0.3;
+                if (assign) {
+                    this.frameBegin = 6;
+                    this.frameEnd = 7;
+                }
+                break;
+            case 'downAirLeft2':
+                this.x = obj.cX - 10;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 10;
+                this.height = 20;
+                this.damage = 20;
+                this.power = 10;
+                if (assign) {
+                    this.frameBegin = 7;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'downAirRight1':
+                this.x = obj.cX + obj.cWidth;
+                this.y = obj.cY + (obj.cHeight / 2) - 10;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 20;
+                this.power = 1;
+                if (assign) {
+                    this.frameBegin = 6;
+                    this.frameEnd = 7;
+                }
+                break;
+            case 'downAirRight2':
+                this.x = obj.cX + obj.cWidth;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 10;
+                this.height = 20;
+                this.damage = 20;
+                this.power = 10;
+                if (assign) {
+                    this.frameBegin = 7;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'backAirLeft1':
+                this.x = obj.cX + obj.cWidth + 1;
+                this.y = obj.cY + (obj.cHeight / 2) - 2;
+                this.width = 11;
+                this.height = 11;
+                this.damage = 15;
+                this.power = 1.9;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'backAirLeft2':
+                this.x = obj.cX + obj.cWidth - 8;
+                this.y = obj.cY + (obj.cHeight / 2) + 3;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 15;
+                this.power = 1.9;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'backAirRight1':
+                this.x = obj.cX - 3;
+                this.y = obj.cY + (obj.cHeight / 2) - 3;
+                this.width = 11;
+                this.height = 11;
+                this.damage = 15;
+                this.power = 1.9;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'backAirRight2':
+                this.x = obj.cX + 8;
+                this.y = obj.cY + (obj.cHeight / 2) + 3;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 15;
+                this.power = 1.9;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'forwardAirLeft':
+                this.x = obj.cX - 10;
+                this.y = obj.cY + (obj.cHeight / 2) + 10;
+                this.width = 15;
+                this.height = 10;
+                this.damage = 8;
+                this.power = 1;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'forwardAirRight':
+                this.x = obj.cX + obj.cWidth - 4;
+                this.y = obj.cY + (obj.cHeight / 2) + 10;
+                this.width = 15;
+                this.height = 10;
+                this.damage = 8;
+                this.power = 1;
+                if (assign) {
+                    this.frameBegin = 2;
+                    this.frameEnd = 4;
+                }
+                break;
+            case 'upSmashLeft1':
+                this.x = obj.cX + 8;
+                this.y = obj.cY;
+                this.width = obj.cWidth - 7;
+                this.height = 5;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'upSmashLeft2':
+                this.x = obj.cX + 3;
+                this.y = obj.cY + 15;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'upSmashRight1':
+                this.x = obj.cX + 8;
+                this.y = obj.cY;
+                this.width = obj.cWidth - 7;
+                this.height = 5;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'upSmashRight2':
+                this.x = obj.cX + obj.cWidth - 4;
+                this.y = obj.cY + 15;
+                this.width = 10;
+                this.height = 10;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'downSmash1':
+                this.x = obj.cX - 10;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 20;
+                this.height = 10;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 7;
+                }
+                break;
+            case 'downSmash2':
+                this.x = obj.cX + obj.cWidth - 5;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 20;
+                this.height = 10;
+                this.damage = 14;
+                this.power = 3;
+                if (assign) {
+                    this.frameBegin = 5;
+                    this.frameEnd = 7;
+                }
+                break;
+            case 'leftSmash1':
+                this.x = obj.cX - 8;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 15;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 2.4;
+                if (assign) {
+                    this.frameBegin = 4;
+                    this.frameEnd = 5;
+                }
+                break;
+            case 'leftSmash2':
+                this.x = obj.cX - 8;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 15;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 2.4;
+                if (assign) {
+                    this.frameBegin = 7;
+                    this.frameEnd = 8;
+                }
+                break;
+            case 'rightSmash1':
+                this.x = obj.cX + obj.cWidth - 3;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 15;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 2.4;
+                if (assign) {
+                    this.frameBegin = 4;
+                    this.frameEnd = 5;
+                }
+                break;
+            case 'rightSmash2':
+                this.x = obj.cX + obj.cWidth - 3;
+                this.y = obj.cY + (obj.cHeight / 2);
+                this.width = 15;
+                this.height = 10;
+                this.damage = 13;
+                this.power = 2.4;
+                if (assign) {
+                    this.frameBegin = 7;
+                    this.frameEnd = 8;
+                }
+                break;
+        }
+    }
+}
+
+export default Hitbox;
---
+++ Services/PF-Fighter/src/game/physics.js
@@ -0,0 +1,236 @@
+class Physic {
+    constructor(gravity, map) {
+        this.gravity = gravity;
+        this.knockbackScaling = 0.1;
+        this.baseKnockback = 5;
+        this.gravityAccumulator = 0;
+        this.stage = map;
+        this.highPosition = this.stage.groundY - (this.stage.height * 0.75);
+        this.lowPosition = this.stage.groundY - (this.stage.height * 0.25);
+    }
+
+    applyGravity(object) {
+        if (object.hitboxes.length > 0) {
+            object.hitboxes.forEach((hitbox) => {
+                let gravityMultiplier = 1;
+                switch (hitbox.dir) {
+                    case 'backAirLeft1':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'backAirLeft2':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'backAirRight1':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'backAirRight2':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'downAirLeft1':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'downAirLeft2':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'downAirRight1':
+                        gravityMultiplier = 0.3;
+                        break;
+                    case 'downAirRight2':
+                        gravityMultiplier = 0.3;
+                        break;
+                }
+                object.velocityY += this.gravity * gravityMultiplier;
+            });
+
+        } else if (object.knockbackFrames > 0) {
+            let gravityMultiplier;
+
+            if (object.cY < this.highPosition) {
+                gravityMultiplier = 0.3;
+            } else if (object.cY > this.lowPosition) {
+                gravityMultiplier = 0.7;
+            } else {
+                gravityMultiplier = 0.5;
+            }
+
+            object.velocityY += this.gravity * gravityMultiplier;
+        } else{
+            this.gravityAccumulator += this.gravity;
+            if (this.gravityAccumulator >= 0.1) {
+                object.velocityY += this.gravityAccumulator;
+                this.gravityAccumulator = 0;
+            }
+        }
+    }
+
+    applyMovement(object) {
+        object.x += object.velocityX;
+        object.y += object.velocityY;
+
+        const friction = 0.95;
+
+        if (Math.abs(object.velocityX) < 0.1) {
+            object.velocityX = 0;
+        } else {
+            object.velocityX *= friction;
+        }
+
+        if (Math.abs(object.velocityY) < 0.1) {
+            object.velocityY = 0;
+        } else {
+            object.velocityY *= friction;
+        }
+    }
+
+    handleCollisionWall(object, canvas) {
+        return object.cY < 0 || object.cY + object.cHeight > canvas.height ||
+            object.cX < 0 || object.cX + object.cWidth > canvas.width;
+    }
+
+    detectHit(hitbox, heartbox) {
+        return hitbox.x < heartbox.cX + heartbox.cWidth &&
+            hitbox.x + hitbox.width > heartbox.cX &&
+            hitbox.y < heartbox.cY + heartbox.cHeight &&
+            hitbox.y + hitbox.height > heartbox.cY;
+    }
+
+    handleHit(object, vs) {
+        if (object.hitboxes.length > 0) {
+            object.hitboxes.forEach((hitbox) => {
+                if (hitbox.isActive && this.detectHit(hitbox, vs)) {
+                    this.applyKnockback(vs, hitbox);
+                }
+            });
+        }
+    }
+
+    applyKnockback(object, hitbox) {
+        if (object.stunFrames === 0) {
+            const attackPower = hitbox.power || 1;
+
+            let knockback = (this.baseKnockback + (object.percent * this.knockbackScaling)) * attackPower;
+
+            object.percent += hitbox.damage;
+
+            object.knockbackFrames = 20;
+            object.stunFrames = Math.floor(object.percent * 0.3);
+            let direction = {x: 0, y: 0};
+            switch (hitbox.dir) {
+                case 'upAirLeft1':
+                    direction.x = -0.3;
+                    direction.y = -1;
+                    break;
+                case 'upAirLeft2':
+                    direction.x = -0.3;
+                    direction.y = -1;
+                    break;
+                case 'upAirRight1':
+                    direction.x = 0.3;
+                    direction.y = -1;
+                    break;
+                case 'upAirRight2':
+                    direction.x = 0.3;
+                    direction.y = -1;
+                    break;
+                case 'downAirLeft1':
+                    direction.x = -0.5;
+                    direction.y = -0.2;
+                    break;
+                case 'downAirLeft2':
+                    direction.x = -0.2;
+                    direction.y = 1;
+                    break;
+                case 'downAirRight1':
+                    direction.x = 0.5;
+                    direction.y = -0.2;
+                    break;
+                case 'downAirRight2':
+                    direction.x = 0.2;
+                    direction.y = 1;
+                    break;
+                case 'backAirLeft1':
+                    direction.x = 1;
+                    direction.y = -0.4;
+                    break;
+                case 'backAirLeft2':
+                    direction.x = 0.5;
+                    direction.y = -0.4;
+                    break;
+                case 'backAirRight1':
+                    direction.x = -1;
+                    direction.y = -0.4;
+                    break;
+                case 'backAirRight2':
+                    direction.x = -0.5;
+                    direction.y = -0.4;
+                    break;
+                case 'forwardAirLeft':
+                    direction.x = -1;
+                    direction.y = 0.2;
+                    break;
+                case 'forwardAirRight':
+                    direction.x = 1;
+                    direction.y = 0.2;
+                    break;
+                case 'upSmashLeft1':
+                    direction.x = -0.8;
+                    direction.y = -1;
+                    break;
+                case 'upSmashLeft2':
+                    direction.x = -0.8;
+                    direction.y = -1;
+                    break;
+                case 'upSmashRight1':
+                    direction.x = 0.8;
+                    direction.y = -1;
+                    break;
+                case 'upSmashRight2':
+                    direction.x = 0.8;
+                    direction.y = -1;
+                    break;
+                case 'downSmash1':
+                    direction.x = -1;
+                    direction.y = -0.3;
+                    break;
+                case 'downSmash2':
+                    direction.x = 1;
+                    direction.y = 0.3;
+                    break;
+                case 'leftSmash1':
+                    direction.x = -1;
+                    direction.y = -0.4;
+                    break;
+                case 'leftSmash2':
+                    direction.x = -1;
+                    direction.y = -0.4;
+                    break;
+                case 'rightSmash1':
+                    direction.x = 1;
+                    direction.y = -0.4;
+                    break;
+                case 'rightSmash2':
+                    direction.x = 1;
+                    direction.y = -0.4;
+                    break;
+            }
+            object.velocityX = (knockback / object.knockbackFrames) * direction.x;
+            object.velocityY = (knockback / object.knockbackFrames) * direction.y;
+
+            object.asHit = false;
+        }
+    }
+
+    handleKnockbackAndStun(object) {
+        if (object.knockbackFrames > 0) {
+            object.x += object.velocityX;
+            object.y += object.velocityY;
+            object.knockbackFrames--;
+        } else if (object.stunFrames > 0) {
+            object.stunFrames--;
+        }
+    }
+
+
+}
+
+export default Physic
