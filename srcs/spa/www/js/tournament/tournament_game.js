347a9a505f08c77b14658707073731afc185b453
Dumno-01 <f.freze@hotmail.fr>
2025-02-22 16:04:05 +0100

    fix css trounament

--- srcs/spa/www/js/tournament/tournament_game.js
+++ srcs/spa/www/js/tournament/tournament_game.js
@@ -179,7 +179,6 @@ class TournamentGame {
 		const container = document.querySelector('.tournament-container');
 		container.innerHTML = `
 			<div class="error-message">
-				<h3>Error</h3>
 				<p>${message}</p>
 			</div>
 		`;
