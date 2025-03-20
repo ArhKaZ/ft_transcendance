ffb14d0f3cffadbe8b19dd80ee61da56162235e5
Sylvain Luiset <syluiset@z3r6p1.42lyon.fr>
2025-03-20 13:43:31 +0100

    modal for verif erase work.

--- srcs/spa/www/js/user/edit_user.js
+++ srcs/spa/www/js/user/edit_user.js
@@ -1,92 +1,90 @@
 import { ensureValidToken } from '/js/utils.js';
 
-validationErase = false;
-
 document.getElementById('userForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    const formData = new FormData(this);
+	const formData = new FormData(this);
 
-    try {
-        await ensureValidToken();
-        const response = await fetch('/api/edit_user_api/', {
-            method: 'PATCH',
+	try {
+		await ensureValidToken();
+		const response = await fetch('/api/edit_user_api/', {
+			method: 'PATCH',
 			headers: {
 				'Authorization' : `Bearer ${sessionStorage.getItem('access_token')}`,
 			},
-            body: formData,
-        });
+			body: formData,
+		});
 
-        const data = await response.json();
-        
-        if (response.ok) {
-            displayMessage(data.message, 'success');
-            setTimeout(() => window.location.href = '/home/', 2000);
-        } else {
-            let errorMessage = 'An error occurred';
-            if (data.error) {
-                const cleanError = data.error
-                    .replace('[ErrorDetail(string=\'', '')
-                    .replace('\', code=\'invalid\')]', '')
-                errorMessage = `Error: ${cleanError}`;
-            } else {
-            errorMessage = `Error:<br>${data.error}`;
-        }
-        displayMessage(errorMessage, 'error');
-        }
-    } catch (error) {
-        displayMessage('A network error occurred.', 'error');
-        console.error('Error details:', error);
-    }
+		const data = await response.json();
+		
+		if (response.ok) {
+			displayMessage(data.message, 'success');
+			setTimeout(() => window.location.href = '/home/', 2000);
+		} else {
+			let errorMessage = 'An error occurred';
+			if (data.error) {
+				const cleanError = data.error
+					.replace('[ErrorDetail(string=\'', '')
+					.replace('\', code=\'invalid\')]', '')
+				errorMessage = `Error: ${cleanError}`;
+			} else {
+			errorMessage = `Error:<br>${data.error}`;
+		}
+		displayMessage(errorMessage, 'error');
+		}
+	} catch (error) {
+		displayMessage('A network error occurred.', 'error');
+		console.error('Error details:', error);
+	}
 });
 
 
 document.getElementById('erase-button').addEventListener('click', async () => {
-    let cancel = false;
-    document.getElementById('modal-btn-yes').addEventListener('click', validationErase = true);
-    document.getElementById('modal-btn-no').addEventListener('click', cancel = true);
-    while (!validationErase) {
-        if (cancel)
-            return;
-    }
-    try {
-        await ensureValidToken();
-        const response = await fetch('/api/erase/', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`,
-            },
-            credentials: 'include',
-        });
-        sessionStorage.removeItem('access_token');
+	document.getElementById('modal-erase').style.display = 'block';
+	document.getElementById('modal-btn-yes').addEventListener('click', async () => {
+		try {
+			await ensureValidToken();
+			const response = await fetch('/api/erase/', {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`,
+				},
+				credentials: 'include',
+			});
+			sessionStorage.removeItem('access_token');
 
-        if (response.ok) {
-        } else {
-            console.error('Error erasing:', response);
-        }
-        window.location.reload();
-    } catch (error) {
-        console.error ('Error', error);
-    }
+			if (response.ok) {
+			} else {
+				console.error('Error erasing:', response);
+			}
+			window.location.reload();
+		} catch (error) {
+			console.error ('Error', error);
+		}
+	});
+
+	document.getElementById('modal-btn-no').addEventListener('click', () => {
+		document.getElementById('modal-erase').style.display = 'none';
+	});
 });
 
 document.getElementById('avatar').addEventListener('change', function() {
-    const fileName = this.files[0] ? this.files[0].name : "No file chosen";
-    document.getElementById('file-chosen').textContent = fileName;
+	const fileName = this.files[0] ? this.files[0].name : "No file chosen";
+	document.getElementById('file-chosen').textContent = fileName;
 });
 
 document.getElementById('return-button').addEventListener('click', () => {
-    window.location.href = "/home/";
+	window.location.href = "/home/";
 });
 
 function displayMessage(message, type) {
-    const messageDiv = document.getElementById('message');
-    if (messageDiv) {
-        messageDiv.innerHTML = message;
-        messageDiv.style.color = type === 'error' ? 'red' : 'green';
-    } else {
-        console.error('Message div not found');
-    }
+	const messageDiv = document.getElementById('message');
+	if (messageDiv) {
+		messageDiv.innerHTML = message;
+		messageDiv.style.color = type === 'error' ? 'red' : 'green';
+	} else {
+		console.error('Message div not found');
+	}
 }
 
