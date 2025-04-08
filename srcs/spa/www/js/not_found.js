43798aadb93a32b09193a0f8969c87618b97bf56
Lucien Gabet <lgabet@z3r3p6.42lyon.fr>
2025-04-08 15:28:33 +0200

    user not found popstate

--- srcs/spa/www/js/not_found.js
+++ srcs/spa/www/js/not_found.js
@@ -1,6 +1,13 @@
 import { router } from './router.js';
 
 export function init () {
+    const popstateHandler = () => {
+        if (window.location.pathname === "/user_not_found/") {
+            router.handReload("/home/");
+        }
+        return ;
+    };
+	window.addEventListener('popstate', popstateHandler);
     document.getElementById('return-button').addEventListener('click', () => {
         router.navigateTo("/home/");
     });
